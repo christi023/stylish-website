d8db499c549242015225c245902685719e696a99
Sheliann Olsson <sheliannohlsson@yahoo.com>
2020-09-03 13:39:53 +0200

    Pricing cards & details

--- src/Components/Pricing/Pricing.jsx
+++ src/Components/Pricing/Pricing.jsx
@@ -16,7 +16,7 @@ export default function Pricing() {
       <div>
         <div className="pricing__section">
           <div className="pricing__wrapper">
-            <h1 className="pricing__header">Pricing</h1>
+            <h1 className="pricing__heading">Pricing</h1>
             <div className="pricing__container">
               <Link to="/sign-up" className="pricing__container-card">
                 <div className="pricing__container-cardInfo">
